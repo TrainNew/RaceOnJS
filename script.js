d4875bd76b7c22d8dc689f0ba1ed0a328432cecb
MIGHOST <54030440+MIGHOST@users.noreply.github.com>
2020-09-09 16:44:08 +0300

    add_hw_2

--- script.js
+++ script.js
@@ -16,18 +16,58 @@ const setting = {
   start: false,
   score: 0,
   speed: 3,
+  traffic: 3,
 };
 
 const startGame = () => {
   start.classList.add("hide");
+  for (let i = 0; i < getQuantityElements(105); i++) {
+    const line = document.createElement("div");
+    line.classList.add("line");
+    line.style.top = i * 105 + "px";
+    line.y = i * 105;
+    gameArea.appendChild(line);
+  }
+
+  for (let i = 0; i < getQuantityElements(120 * setting.traffic); i++) {
+    const enemies = document.createElement("div");
+    enemies.classList.add("enemy");
+    enemies.y = -120 * setting.traffic * (i + 1);
+    enemies.style.left =
+      Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + "px";
+    enemies.style.top = enemies.y + "px";
+    enemies.style.background =  'transparent url(../image/E.png) center / cover no-repeat';
+    gameArea.appendChild(enemies);
+  }
   setting.start = true;
   gameArea.appendChild(car);
+  setting.x = car.offsetLeft;
+  setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
 };
 
 const playGame = () => {
-  console.log("Play game!");
+  moveRoad();
+  moveEnemy();
   if (setting.start) {
+    if (keys.ArrowLeft && setting.x > 0) {
+      setting.x -= setting.speed;
+    }
+
+    if (keys.ArrowRight && setting.x < gameArea.offsetWidth - car.offsetWidth) {
+      setting.x += setting.speed;
+    }
+    if (keys.ArrowUp && setting.y > 0) {
+      setting.y -= setting.speed;
+    }
+    if (
+      keys.ArrowDown &&
+      setting.y < gameArea.offsetHeight - car.offsetHeight
+    ) {
+      setting.y += setting.speed;
+    }
+    car.style.left = setting.x + "px";
+    car.style.top = setting.y + "px";
     requestAnimationFrame(playGame);
   }
 };
@@ -41,6 +81,32 @@ const stopRun = (e) => {
   keys[e.key] = false;
 };
 
+const moveRoad = () => {
+  let lines = document.querySelectorAll(".line");
+  lines.forEach((line) => {
+    line.y += setting.speed;
+    line.style.top = line.y + "px";
+    if (line.y >= document.documentElement.clientHeight) {
+      line.y = -100;
+    }
+  });
+};
+
+const moveEnemy = () => {
+  let enemy = document.querySelectorAll(".enemy");
+  enemy.forEach((i) => {
+    i.y += setting.speed / 2;
+    i.style.top = i.y + "px";
+    if (i.y >= document.documentElement.clientHeight) {
+      i.y = -120 * setting.traffic;
+      i.style.left =
+        Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + "px";
+    }
+  });
+};
+
+const getQuantityElements = (heightElement) =>
+  document.documentElement.clientHeight / heightElement + 1;
 
 start.addEventListener("click", startGame);
 document.addEventListener("keydown", startRun);
